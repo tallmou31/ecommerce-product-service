7fe8de2169711695fb43cb5e2cefe5ac72ab1d86
Mouhamed Lamine TALL [SNT DSI/INS/IMOC] <mouhamedlamine.tall1@orange-sonatel.com>
2023-11-26 12:28:56 +0000

    Initial version of productservice generated by generator-jhipster@7.9.4

---
+++ src/main/java/sn/esmt/mp2isi/config/Constants.java
@@ -0,0 +1,11 @@
+package sn.esmt.mp2isi.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
